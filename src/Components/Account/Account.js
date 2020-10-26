a1cac2269623b2d0026dc24e66982cc6fea80357
DylanSironi <sironidylan@gmail.com>
2020-10-26 15:10:28 -0500

    account-updates

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './account.css'
 
 class Account extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+        }
+    }
     render() {
+        console.log(this.props)
         return (
-            <section className='account'>
-                <div>
-                    <p>username</p>
-                    <p>email</p>
+            <section className='account-section'>
+                <button className='account-menu'>
+                    <p>Your current Username is {this.props.user.username}</p>
                     <input placeholder='change username'/>
-                    <input placeholder='change email'/>
-
-                </div>
+                    <button>Change</button>
+                    <p>Your current Email is {this.props.user.email}</p>
+                    <input className='email-input' placeholder='change email'/>
+                    <button>Change</button>
+                </button>
             </section>
         )
     }
 }
-export default Account
+const mapStateToProps = reduxState => reduxState;
+export default connect(mapStateToProps)(Account);
